9aad79452de868662a32b442171010ed6b774b77
Dani <thos.merida.dani@gmail.com>
2024-11-28 17:01:48 +0100

    home commit

--- app/src/main/java/com/example/m7animedex/MainActivity.kt
+++ app/src/main/java/com/example/m7animedex/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.m7animedex
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.ViewCompat.setOnApplyWindowInsetsListener
 import androidx.core.view.WindowInsetsCompat
+import com.example.m7animedex.R.id.main
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setOnApplyWindowInsetsListener(findViewById(main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
