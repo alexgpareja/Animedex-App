f13ee98d76a55dc219026188b1f18d550b35f73e
Alex Gil <alexgpareja@icloud.com>
2024-12-04 19:18:15 +0100

    afegit les noves trucades del header y el footer correctament

--- app/src/main/java/com/example/m7animedex/FooterFragment.kt
+++ app/src/main/java/com/example/m7animedex/FooterFragment.kt
@@ -44,7 +44,7 @@ class FooterFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment FooterFragment.
+         * @return A new instance of fragment FooterFragment2.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
