fa4ce008d7915d6b7b6bb112297270e0d924d6cb
Dani <thos.merida.dani@gmail.com>
2025-01-23 18:12:21 +0100

    RecyclerView commit

--- app/src/main/java/com/example/m7animedex/MainActivity.kt
+++ app/src/main/java/com/example/m7animedex/MainActivity.kt
@@ -1,19 +1,12 @@
 package com.example.m7animedex
 
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.m7animedex.R.id.main
-import org.json.JSONArray
-import java.net.URL
-import android.graphics.BitmapFactory
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 
 class MainActivity : AppCompatActivity() {
     private lateinit var topAiringRecyclerView: RecyclerView
@@ -32,19 +25,15 @@ class MainActivity : AppCompatActivity() {
             .replace(R.id.header, FragmentHeader())
             .commit()
 
-        // Añadir el Bottom Navigation Fragment
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.footer, BottomNavigationFragment())
-            .commit()
+        // Configurar LayoutManager para que los RecyclerView sean horizontales
+        topAiringRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        mostPopularRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
 
-        // Configurar LayoutManager
-        topAiringRecyclerView.layoutManager =  GridLayoutManager(this, 3)
-        mostPopularRecyclerView.layoutManager = GridLayoutManager(this,3)
+
 
         // Configurar el Adapter con la lista de datos
         topAiringRecyclerView.adapter = AnimeAdapter(AnimeProvider.Animes)
         mostPopularRecyclerView.adapter = AnimeAdapter(AnimeProvider.Animes)
-
 
         // Configurar insets para Edge-to-Edge
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
