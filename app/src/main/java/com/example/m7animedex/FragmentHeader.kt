e28f4fdd7dd80f9d47180bf9c6833bd8e256c91d
Dani <thos.merida.dani@gmail.com>
2025-03-18 19:10:28 +0100

    funcionamiento header

--- app/src/main/java/com/example/m7animedex/FragmentHeader.kt
+++ app/src/main/java/com/example/m7animedex/FragmentHeader.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class FragmentHeader : Fragment() {
@@ -14,16 +15,26 @@ class FragmentHeader : Fragment() {
     ): View {
         val view = inflater.inflate(R.layout.fragment_header, container, false)
 
-        // Referencia al avatar
         val avatarIcon = view.findViewById<ImageView>(R.id.avatar_icon)
+        val logoImage = view.findViewById<ImageView>(R.id.imageView4)
+        val headerTitle = view.findViewById<TextView>(R.id.tvHeaderTitle)
 
-        // Configurar el clic para abrir el SettingsFragment
         avatarIcon.setOnClickListener {
             parentFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, SettingsFragment())
-                .addToBackStack(null) // Permite volver atrás con el botón de retroceso
+                .addToBackStack(null)
                 .commit()
         }
+
+        val navigateToHome = View.OnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HomeFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+
+        logoImage.setOnClickListener(navigateToHome)
+        headerTitle.setOnClickListener(navigateToHome)
 
         return view
     }
